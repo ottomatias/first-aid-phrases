d7c6b0c3659d0abfd0043693066913688948b2e9
Matthew Higgins <matthew@matthewhiggins.me>
2016-01-01 22:04:50 +0200

    Fix GA View Tracking

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,6 +4,6 @@ $('document').ready(function(){
 	$('.translation_link').click(function(){
 		$('#phrase_body').html($(this).data('translation'));
     virtual_url = window.location.pathname + "#modal-" + $(this).attr('id');
-    ga('send','pageview', virtual_url);
+    $('#phrase_body').data('virtual-url', virtual_url)
 	})
 })
